350dea980773aea53de965571d992b488f63c0ec
Deepa <deepamaria605@gmail.com>
2023-12-16 22:44:22 +0100

    Modified Navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,51 +5,58 @@ function Nav() {
   return (
     <div className='Nav'>
         <div className="navbar h-full bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
+          <div className="navbar-start h-full">
+            <img className='img h-full' src='logo.png' alt='logo'></img>
+            <a className="btn btn-ghost text-xl">Scheduler4U</a>
+          </div>
+            
+          <div className="navbar-end">
+            <div className="join join-horizontal lg:join-horizontal">
+                <ion-icon size="large" className="join-item" name="globe-outline"></ion-icon>
+                <span><button className='join-item'>
+                    <select className="select select-bordered w-full max-w-xs">
+                        {/* Language need not be selected only disabled */}
+                        <option disabled selected>Language</option>
+                        <option selected>EN</option>
+                        <option>DE</option>
+                    </select>
+                </button></span>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    )
+  }
+            
+  export default Nav
+  
+  
+  
+  {/* <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-32">
         <li><Link to={"/login"}><a>LogIn</a></Link></li>
         <li><Link to={"/signup"}><a><button className='btn btn-primary'>SignUp</button></a></Link></li>
-        {/* create an About page regarding the website */}
-        {/* <li><a>About</a></li> */}
+        create an About page regarding the website
+        <li><a>About</a></li>
       </ul>
-    </div>
-  </div>
-  <div className="navbar-center h-full">
+    </div> */}
+
+  {/* <div className="navbar-center h-full">
     <img className='img h-full' src='logo.png' alt='logo'></img>
-    <a className="btn btn-ghost text-xl">Godis Great | Scheduler4U</a>
-  </div>
-  <div className="navbar-end">
+    <a className="btn btn-ghost text-xl">Scheduler4U</a>
+  </div> */}
 
     {/* <button className="btn btn-ghost btn-circle">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-    </button>
-     */}
+      </button>
+    */}
     {/* <button className="btn btn-ghost btn-circle">
       <div className="indicator">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-        <span className="badge badge-xs badge-primary indicator-item"></span>
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+      <span className="badge badge-xs badge-primary indicator-item"></span>
       </div>
     </button> */}
-    <div className="join join-horizontal lg:join-horizontal">
-        <ion-icon size="large" className="join-item" name="globe-outline"></ion-icon>
-        <span><button className='join-item'>
-            <select className="select select-bordered w-full max-w-xs">
-                {/* Language need not be selected only disabled */}
-                <option disabled selected>Language</option>
-                <option selected>English</option>
-                <option>Deutsch</option>
-            </select>
-        </button></span>
-    </div>
-
-  </div>
-</div>
-    </div>
-  )
-}
-
-export default Nav
