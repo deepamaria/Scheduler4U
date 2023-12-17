f84c565f7090848e3b3099fceeefcb13bdf4dad0
Deepa <deepamaria605@gmail.com>
2023-12-17 03:46:40 +0100

    Created registration form and set a link to login

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,37 +1,49 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function SignUpPage() {
   return (
     <div className='SignUpPage flex h-full w-full'>
-        <div className='SignUp h-full w-1/3'>
-            <h1 className='text-2xl font-bold text-center'>Create Your Account</h1>
-        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+
+        <div className='SignUp h-full w-full'>
+            <h1 className='text-2xl font-bold text-center'>Sign Up for your Scheduler4U Account</h1>
+        <div className="card w-full max-w-sm shadow-2xl bg-cyan-500">
+      
       <form className="card-body">
+      
       <div className="form-control">
-          <label className="label">
+          {/* <label className="label">
             <span className="label-text">FullName</span>
-          </label>
-          <input type="text" placeholder="your Full Name" name='fullname' className="input input-bordered" required />
+          </label> */}
+          <input type="text" id='fullname' name='fullname' placeholder="FullName" className="input input-bordered" required />
         </div>
 
         <div className="form-control">
-          <label className="label">
+          {/* <label className="label">
             <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" name='email' className="input input-bordered" required />
+          </label> */}
+          <input type="email" id='email' name='email' placeholder="Email" className="input input-bordered" required />
         </div>
+
         <div className="form-control">
-          <label className="label">
+          {/* <label className="label">
             <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password (Atleast 8 charachters)" name='password' className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
+          </label> */}
+          <input type="password" id='password' name='password' placeholder="Password" className="input input-bordered" required />
         </div>
+
         <div className="form-control mt-6">
-          <button className="btn btn-primary">SignUp</button>
+          <button type='submit' className="btn btn-cyan-600">SignUp</button>
         </div>
+        
+        <div className='hr flex'>
+          <p>OR</p>
+        </div>
+
+        <Link to={"/login"}><div className='form-control btn-cyan-600 btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-gradient-to-r from-cyan-500 shadow-2xl shadow-cyan-500/50'>Log In</div></Link>
+
+
+
       </form>
         </div>
         </div>
