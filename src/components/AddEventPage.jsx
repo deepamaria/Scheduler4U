80f395c131bc8879da582d469d6f4e436bc33a3f
Deepa <deepamaria605@gmail.com>
2023-12-17 06:38:56 +0100

    Add Event Component was created

--- src/components/AddEventPage.jsx
+++ src/components/AddEventPage.jsx
@@ -8,7 +8,45 @@ function AddEventPage() {
   return (
     <div>AddEventPage
 
+div className="card w-full max-w-sm shadow-2xl bg-cyan-500">
+      
+      <form className="card-body">
+      
+      <div className="form-control">
+          {/* <label className="label">
+            <span className="label-text">FullName</span>
+          </label> */}
+          <input type="text" id='fullname' name='fullname' placeholder="FullName" className="input input-bordered" required />
+        </div>
 
+        <div className="form-control">
+          {/* <label className="label">
+            <span className="label-text">Email</span>
+          </label> */}
+          <input type="email" id='email' name='email' placeholder="Email" className="input input-bordered" required />
+        </div>
+
+        <div className="form-control">
+          {/* <label className="label">
+            <span className="label-text">Password</span>
+          </label> */}
+          <input type="password" id='password' name='password' placeholder="Password" className="input input-bordered" required />
+        </div>
+
+        <div className="form-control mt-6">
+          <button type='submit' className="btn btn-cyan-600 bg-cyan-600">SignUp</button>
+        </div>
+        
+        <div className='hr flex'>
+          <p>OR</p>
+        </div>
+
+        <Link to={"/login"}><div className='btn btn-cyan-600 btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-gradient-to-r from-cyan-500 shadow-2xl shadow-cyan-500/50'>Log In</div></Link>
+
+
+
+      </form>
+        </div>
 
     </div>
   )
